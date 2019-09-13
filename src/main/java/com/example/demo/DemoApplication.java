4e2dbbefd5894e6c66ba7b54304138b7c2c83ccf
salaboy <salaboy@gmail.com>
2019-09-13 11:44:55 +0200

    initial commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,53 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.zeebe.spring.client.EnableZeebeClient;
+import io.zeebe.spring.client.annotation.ZeebeWorker;
+import lombok.extern.slf4j.Slf4j;
+import io.zeebe.client.api.worker.JobClient;
+import io.zeebe.client.api.response.ActivatedJob;
+
+import java.time.Instant;
 
 @SpringBootApplication
+@EnableZeebeClient
+@Slf4j
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@ZeebeWorker(type = "classify")
+	public void classifyEmergency(final JobClient client, final ActivatedJob job) {
+		logJob(job);
+		client.newCompleteCommand(job.getKey()).variables("{\"foo\": 1}").send().join();
+	}
+
+
+	@ZeebeWorker(type = "hospital")
+	public void handleHospitalCoordination(final JobClient client, final ActivatedJob job) {
+		logJob(job);
+		client.newCompleteCommand(job.getKey()).variables("{\"foo\": 1}").send().join();
+	}
+
+	@ZeebeWorker(type = "firefighter")
+	public void handleFirefighterCoordination(final JobClient client, final ActivatedJob job) {
+		logJob(job);
+		client.newCompleteCommand(job.getKey()).variables("{\"foo\": 1}").send().join();
+	}
+
+
+	private static void logJob(final ActivatedJob job) {
+		log.info(
+				"complete job\n>>> [type: {}, key: {}, element: {}, workflow instance: {}]\n{deadline; {}]\n[headers: {}]\n[variables: {}]",
+				job.getType(),
+				job.getKey(),
+				job.getElementId(),
+				job.getWorkflowInstanceKey(),
+				Instant.ofEpochMilli(job.getDeadline()),
+				job.getCustomHeaders(),
+				job.getVariables());
 	}
 
 }
