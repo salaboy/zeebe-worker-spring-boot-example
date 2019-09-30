2a2942e94a96b819ced6c9c1879ad0670fc8d640
salaboy <salaboy@gmail.com>
2019-09-30 08:41:10 +0100

    small refactoring

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,40 +15,46 @@ import java.time.Instant;
 @Slf4j
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@ZeebeWorker(type = "classify")
-	public void classifyEmergency(final JobClient client, final ActivatedJob job) {
-		logJob(job);
-		client.newCompleteCommand(job.getKey()).variables("{\"emergencyType\": \"Injured\"}").send().join();
-	}
-
-
-	@ZeebeWorker(type = "hospital")
-	public void handleHospitalCoordination(final JobClient client, final ActivatedJob job) {
-		logJob(job);
-		client.newCompleteCommand(job.getKey()).send().join();
-	}
-
-	@ZeebeWorker(type = "firefighter")
-	public void handleFirefighterCoordination(final JobClient client, final ActivatedJob job) {
-		logJob(job);
-		client.newCompleteCommand(job.getKey()).send().join();
-	}
+    @ZeebeWorker(type = "classify")
+    public void classifyEmergency(final JobClient client, final ActivatedJob job) {
+        logJob(job);
+        if (job.getVariablesAsMap().get("emergencyReason") == null) { // default to ambulance if no reason is provided
+            client.newCompleteCommand(job.getKey()).variables("{\"emergencyType\": \"Injured\"}").send().join();
+        }else if (job.getVariablesAsMap().get("emergencyReason").toString().contains("person")) {
+            client.newCompleteCommand(job.getKey()).variables("{\"emergencyType\": \"Injured\"}").send().join();
+        } else if (job.getVariablesAsMap().get("emergencyReason").toString().contains("fire")) {
+            client.newCompleteCommand(job.getKey()).variables("{\"emergencyType\": \"Fire\"}").send().join();
+        }
+    }
 
 
-	private static void logJob(final ActivatedJob job) {
-		log.info(
-				"complete job\n>>> [type: {}, key: {}, element: {}, workflow instance: {}]\n{deadline; {}]\n[headers: {}]\n[variables: {}]",
-				job.getType(),
-				job.getKey(),
-				job.getElementId(),
-				job.getWorkflowInstanceKey(),
-				Instant.ofEpochMilli(job.getDeadline()),
-				job.getCustomHeaders(),
-				job.getVariables());
-	}
+    @ZeebeWorker(type = "hospital")
+    public void handleHospitalCoordination(final JobClient client, final ActivatedJob job) {
+        logJob(job);
+        client.newCompleteCommand(job.getKey()).send().join();
+    }
+
+    @ZeebeWorker(type = "firefighters")
+    public void handleFirefighterCoordination(final JobClient client, final ActivatedJob job) {
+        logJob(job);
+        client.newCompleteCommand(job.getKey()).send().join();
+    }
+
+
+    private static void logJob(final ActivatedJob job) {
+        log.info(
+                "complete job\n>>> [type: {}, key: {}, element: {}, workflow instance: {}]\n{deadline; {}]\n[headers: {}]\n[variables: {}]",
+                job.getType(),
+                job.getKey(),
+                job.getElementId(),
+                job.getWorkflowInstanceKey(),
+                Instant.ofEpochMilli(job.getDeadline()),
+                job.getCustomHeaders(),
+                job.getVariables());
+    }
 
 }
